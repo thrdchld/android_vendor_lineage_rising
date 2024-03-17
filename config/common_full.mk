d1593cb41b23dbd320d0bcd82e376c64ffd1a344
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-18 02:31:50 +0800

    config: Guard lineage prebuilts with GMS flags
    Change-Id: I7ff6cf3a88622284dc21d22a1960753ce988ff36
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common_full.mk
+++ config/common_full.mk
@@ -3,6 +3,7 @@ $(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
+ifneq ($(WITH_GMS), true)
 # Apps
 PRODUCT_PACKAGES += \
     Eleven \
@@ -10,12 +11,15 @@ PRODUCT_PACKAGES += \
     Profiles \
     Recorder \
     Seedvault
+endif
 
+PRODUCT_NO_CAMERA ?= true
 ifneq ($(PRODUCT_NO_CAMERA),true)
 PRODUCT_PACKAGES += \
     Aperture
 endif
 
+TARGET_EXCLUDES_AUDIOFX ?= true
 ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
 PRODUCT_PACKAGES += \
     AudioFX
