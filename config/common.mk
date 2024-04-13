dcc4333745f414f1f0d4569ea5af4119e9a6fc05
minaripenguin <minaripenguin@users.noreply.github.com>
2024-04-13 09:03:36 +0800

    config: Include risingOS vendor
    Change-Id: I1c51e66c4432f947493e45486ff8bf55176e44d1
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,7 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/crdroid.mk)
 $(call inherit-product-if-exists, vendor/addons/config.mk)
 $(call inherit-product-if-exists, vendor/pixel-framework/config.mk)
+include vendor/rising/config/rising.mk
 
 PRODUCT_BRAND ?= risingOS
 
